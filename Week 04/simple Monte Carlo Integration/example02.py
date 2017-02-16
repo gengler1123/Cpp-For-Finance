24b6d41592dd7ee8d55884f7a255586c0780668e
gengler1123 <gengler@stevens.edu>
2017-02-16 17:44:25 -0500

    Update

--- Week 04/simple Monte Carlo Integration/example02.py
+++ Week 04/simple Monte Carlo Integration/example02.py
@@ -7,7 +7,10 @@ Created on Thu Feb 16 07:26:36 2017
 
 from subprocess import PIPE, run
 
+'''
+The purpose of this code is to approximate the value of Pi.
+'''
 
-R = run(args = ["multiThreading.exe"], stdout = PIPE)
+R = run(args = ["Simple Monte Carlo Integration.exe"], stdout = PIPE)
 
 print(R.stdout.decode('UTF-8'))
